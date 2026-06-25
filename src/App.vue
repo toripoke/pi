9c529169fef98d17bd86d129b4c5ce99fa04fe5d
neko_reset <1866+neko_reset1024@trap.jp>
2026-06-25 14:23:55 +0900

    yokonarabi

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import SideBar from './components/SideBar.vue'
+import SampleComponent from './components/SampleComponent.vue'
 import WelcomeMessage from './components/WelcomeMessage.vue'
+
 </script>
 
 <template>
   <main>
-    <WelcomeMessage msg="Webエンジニアになろう講習会へようこそ" />
+    <div class="horizontal">
+      <SideBar />
+      <WelcomeMessage msg="Webエンジニアになろう講習会へようこそ" />
+      <SampleComponent msg="Webエンジニアになろう講習会へようこそ" />
+    </div>
   </main>
 </template>
 
 <style scoped>
 main {
   padding: 2rem;
+}
+
+.horizontal{
+  display : flex;
+  flex-direction : row;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>SideBar</h1>
+</template>
